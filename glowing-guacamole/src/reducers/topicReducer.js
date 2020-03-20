5ba8ca690fc79c57bd84ebd4711bf2d707052b77
yasmine-hj <yasminehajaji95@gmail.com>
2020-03-20 10:58:58 +0000

    adding topics reducer

---
+++ glowing-guacamole/src/reducers/topicReducer.js
@@ -0,0 +1,40 @@
+export default function categoryReducer(state = {categories: []}, action) {
+    switch (action.type) {
+        case 'FETCH_TOPICS':
+            return {topics: action.payload} 
+
+        case 'CREATE_RESPONSE':
+            let topics = state.topics.map(topic => {
+                if (topic.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return topic}
+                })
+            return {...state, topics: topics}  
+
+        case 'DELETE_RESPONSE':
+            let topicsTwo = state.topics.map(topic => {
+                if (topic.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return topic}
+            })
+            return {...state, topics: topicsTwo}
+            default:
+                return state
+
+        case 'UPDATE_RESPONSE':
+            let topicsThree = state.topics.map(topic => {
+                if (topic.id === action.payload.id) {
+                  return action.payload
+                } else {
+                  return topic
+                }
+              })
+              return {...state, topics: topicsThree}
+            default:
+              return state
+    }
+}
+          
+    
