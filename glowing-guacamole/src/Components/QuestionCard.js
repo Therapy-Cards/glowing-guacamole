9e4862511bb23757516ee0dd2ae0895ff60331cf
yasmine-hj <yasminehajaji95@gmail.com>
2020-04-05 20:18:38 +0100

    add handle change

--- glowing-guacamole/src/Components/QuestionCard.js
+++ glowing-guacamole/src/Components/QuestionCard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '@material-ui/core/Button'
 
 class QuestionCard extends React.Component {
+    
     constructor() {
         super()
         this.state = {
